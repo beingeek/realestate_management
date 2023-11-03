9a51ef92b29c23d7431e45fbbecf31d6fb62b95e
Tariq Siddique <136646271+mtariqsiddique@users.noreply.github.com>
2023-11-03 10:54:37 +0500

    Update plot_list.js

--- realestate_account/realestate_account/doctype/plot_list/plot_list.js
+++ realestate_account/realestate_account/doctype/plot_list/plot_list.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2023, CE Construction and contributors
-// For license information, please see license.txt
-
-
 frappe.ui.form.on('Plot List', {
     refresh: function(frm) {
         var status = frm.doc.status;
