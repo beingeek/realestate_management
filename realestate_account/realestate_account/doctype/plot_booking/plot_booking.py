7b714ada80318486ae041f782315aa431798420b
beingeek <furqan.790@outlook.com>
2023-11-04 14:13:59 +0500

    chore: moved logic to controller

--- realestate_account/realestate_account/doctype/plot_booking/plot_booking.py
+++ realestate_account/realestate_account/doctype/plot_booking/plot_booking.py
@@ -1,7 +1,100 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils import (
+    flt, cstr, today
+)
 
+class ClosedAccountingPeriod(frappe.ValidationError):
+	pass
+
+class PlotBooking(Document):
+    def validate(self):
+        self.validate_booking_date()
+        validate_accounting_period_open(self)
+        self.validate_plot_booking()
+        self.validate_amount()
+
+    def on_submit(self):
+        self.create_invoice()
+        self.book_plot()
+
+    def on_cancel(self):
+        self.unbook_plot()
+
+    def validate_plot_booking(self):
+        if not self.plot_no:
+            frappe.throw(_("Error: Plot number not specified in the Plot Booking document."))
+
+        plot_status = frappe.db.get_value('Plot List', self.plot_no, 'status')
+        if plot_status == 'Booked':
+            frappe.throw(_('The {0} is already booked').format(frappe.get_desk_link('Plot List', self.plot_no)))
+
+    def validate_booking_date(self):
+        if self.booking_date > today():
+            frappe.throw(_('Future booking date not allowed.'))
+
+    def validate_amount(self):
+        if flt(self.difference) != 0.0:
+            frappe.throw(_('Amount of Installment Total and Grand Total is not matched'))
+
+
+    def create_invoice(self):
+        company = frappe.get_doc("Company", self.company)
+        company_default_cost_center = frappe.db.get_value("Company", self.company, 'cost_center')
+        realestate_default_cost_center = frappe.db.get_single_value("Real Estate Setting", "cost_center")
+        default_commission_item = frappe.db.get_single_value("Real Estate Setting", "commission_item")
+        default_cost_center = realestate_default_cost_center or company_default_cost_center
+
+        invoice = frappe.get_doc({
+                    "doctype": "Purchase Invoice",
+                    "supplier": self.sales_broker,
+                    "posting_date": self.booking_date,
+                    "bill_no" : self.plot_no,
+                    "project" : self.project_name,
+                    "cost_centre" : "",
+                    "custom_booking_number":self.name,
+                    "company" : company
+                })
+
+        invoice.append("items", {
+                    "item_code": default_commission_item,
+                    "qty": 1,
+                    "rate": self.commission_amount,
+                    "amount": self.commission_amount,
+                    "project" : self.project_name,
+                    "cost_centre" : default_cost_center
+                })
+
+        invoice.insert(ignore_permissions=True)
+        invoice.submit()
+        frappe.msgprint(_('{0} Broker commission created').format(frappe.get_desk_link('Purchase Invoice', invoice.name)))
+
+    def book_plot(self):
+        plot_master = frappe.get_doc("Plot List", self.plot_no)
+
+        # Check if the plot is available or Hold for Sales
+        if plot_master.status == "Available" and plot_master.hold_for_sale == 0:
+            plot_master.update({
+                'status': "Booked", 'client_name': self.client_name, 'address': self.address,
+                'mobile_no': self.mobile_no, 'sales_agent': self.sales_broker,
+                'father_name': self.father_name, 'cnic': self.cnic,
+            })
+            plot_master.save()
+            frappe.msgprint(_('{0} booked successfully').format(frappe.get_desk_link('Plot List', plot_master.name)))
+        else:
+            frappe.throw(_("Error: The selected plot is not available for booking."))
+
+    def unbook_plot(self):
+        plot_master = frappe.get_doc("Plot List", self.plot_no)
+        plot_master.update({
+            'status': "Available", 'client_name': '', 'address': '',
+            'mobile_no': '', 'sales_agent': '',
+            'father_name': '',  'cnic': ''
+        })
+
+        plot_master.save()
+        frappe.msgprint(_('{0} unbooked').format(frappe.get_desk_link('Plot List', plot_master.name)))
 
 @frappe.whitelist()
 def get_available_plots(project_name):
@@ -34,65 +127,6 @@ def get_plot_detail(plot_no):
         frappe.log_error(frappe.get_traceback(), _("Error in fetch_data"))
         frappe.throw(_("No available plots found: {0}").format(str(e)))
 
-@frappe.whitelist()
-def update_after_submit(plot_booking_name):
-    try:
-        plot_booking = frappe.get_doc("Plot Booking", plot_booking_name)
-
-        if plot_booking.plot_no:
-            plot_master = frappe.get_doc("Plot List", plot_booking.plot_no)
-
-            # Check if the plot is available or Hold for Sales
-            if plot_master.status == "Available" and plot_master.hold_for_sale == 0: 
-                 
-                plot_master.status      = "Booked"
-                plot_master.client_name = plot_booking.client_name
-                plot_master.address     = plot_booking.address
-                plot_master.father_name = plot_booking.father_name
-                plot_master.cnic        = plot_booking.cnic
-                plot_master.mobile_no   = plot_booking.mobile_no
-                plot_master.sales_agent = plot_booking.sales_broker
-
-                plot_master.save()
-                frappe.db.commit()
-
-                return "Success"          
-            else:
-                return "Error: The selected plot is not available for booking."
-
-        else:
-            return "Error: Plot number not specified in the Plot Booking document."
-
-    except Exception as e:
-        frappe.log_error(frappe.get_traceback(), _("Failed to update plot status"))
-        return frappe.get_traceback()
-
-@frappe.whitelist()
-def update_after_cancel(plot_booking_name):
-    try:
-        plot_booking = frappe.get_doc("Plot Booking", plot_booking_name)
-        
-        if plot_booking.plot_no:
-            plot_master = frappe.get_doc("Plot List", plot_booking.plot_no)
-            
-            plot_master.status          = "Available"
-            plot_master.client_name     = ""
-            plot_master.address         = ""
-            plot_master.father_name     = ""
-            plot_master.cnic            = ""
-            plot_master.mobile_no       = ""
-            plot_master.sales_agent     = ""
-            
-            plot_master.save()
-            frappe.db.commit()
-            
-            return "Success"
-        else:
-            return "Plot number not specified in the Plot Booking document."
-
-    except Exception as e:
-        frappe.log_error(frappe.get_traceback(), _("Failed to update plot status"))
-        return "Failed"
 
 @frappe.whitelist()
 def create_invoice(plot_booking):
@@ -130,33 +164,25 @@ def create_invoice(plot_booking):
     except Exception as ex:
         frappe.msgprint(f"Unexpected error: {str(ex)}")
 
+def validate_accounting_period_open(doc, method=None):
+    ap = frappe.qb.DocType("Accounting Period")
+    cd = frappe.qb.DocType("Closed Document")
+    accounting_period = (
+        frappe.qb.from_(ap)
+        .from_(cd)
+        .select(ap.name)
+        .where(
+            (ap.name == cd.parent)
+            & (ap.company == doc.company)
+            & (cd.closed == 1)
+            & (cd.document_type == doc.doctype)
+            & (doc.booking_date >= ap.start_date)
+            & (doc.booking_date <= ap.end_date)
+        )
+    ).run(as_dict=1)
 
-
-@frappe.whitelist()
-def check_accounting_period_open(booking_date):
-    try:
-        sql_query = """
-            SELECT closed
-            FROM `tabAccounting Period` AS tap
-            LEFT JOIN `tabClosed Document` AS tcd ON tcd.parent = tap.name
-            WHERE tcd.document_type = 'Purchase Invoice' 
-                AND MONTH(tap.end_date) = MONTH(%s) 
-                AND YEAR(tap.end_date) = YEAR(%s)
-                LIMIT 1;
-        """
-        result = frappe.db.sql(sql_query, (booking_date, booking_date), as_dict=True)
-
-        if not result:
-            return {'is_open': None}  # Return a dictionary with 'is_open' set to None
-
-        return {'is_open': result[0]['closed']}  # Return a dictionary with 'is_open' value
-    except Exception as e:
-        frappe.log_error(f"Error getting in period: {str(e)}")
-        return {'is_open': None}
-
-
-
-
-
-class PlotBooking(Document):
-    pass
+    if accounting_period:
+        frappe.throw(_("You cannot create a {0} within the closed Accounting Period {1}").format(
+            doc.doctype, frappe.bold(accounting_period[0]["name"]),
+            ClosedAccountingPeriod
+        ))
