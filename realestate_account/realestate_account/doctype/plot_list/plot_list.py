bd873444d1b3cac452dae17321ac4637bf0b197e
beingeek <furqan.790@outlook.com>
2023-11-04 21:50:14 +0500

    feat: added title field in plot booking plot

--- realestate_account/realestate_account/doctype/plot_list/plot_list.py
+++ realestate_account/realestate_account/doctype/plot_list/plot_list.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2023, CE Construction and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class PlotList(Document):
-	pass
+	def validate(self):
+		self.set_title()
+
+	def set_title(self):
+		self.title = _("{0} - {1} - {2}").format(self.plot_name, self.land_price, self.plot_feature)
