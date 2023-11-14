c9fb1f88490164ada4654d9ce4379fef40b6683e
beingeek <furqan.790@outlook.com>
2023-11-14 23:50:07 +0500

    chore: minor refactoring

--- realestate_account/realestate_account/doctype/plot_list/plot_list.js
+++ realestate_account/realestate_account/doctype/plot_list/plot_list.js
@@ -1,32 +1,26 @@
 frappe.ui.form.on('Plot List', {
     refresh: function(frm) {
-        var status = frm.doc.status;
-        frm.toggle_enable("hold_for_sale", status !== "Booked");
-    }
-});
+        frm.toggle_enable("hold_for_sale", frm.doc.status !== "Booked");
+    },
 
-frappe.ui.form.on('Plot List', {
 	hold_for_sale: function(frm) {
-        var holdForSale = frm.doc.hold_for_sale;
-        frm.toggle_display("reason_for_hold", holdForSale);
+        frm.toggle_display("reason_for_hold", frm.doc.hold_for_sale);
     },
 
 	onload: function(frm) {
-        var holdForSale = frm.doc.hold_for_sale;
-        frm.toggle_display("reason_for_hold", holdForSale);
-    }
-});
+        frm.toggle_display("reason_for_hold", frm.doc.hold_for_sale);
+    },
 
-frappe.ui.form.on('Plot List', {
     land_area: function(frm) {
         calculate_total_amount(frm);
     },
+
     land_price: function(frm) {
         calculate_total_amount(frm);
     }
 });
 
 function calculate_total_amount(frm) {
-    var  total_amount = parseFloat(frm.doc.land_area) * parseFloat(frm.doc.land_price);
+    var total_amount = parseFloat(frm.doc.land_area) * parseFloat(frm.doc.land_price);
     frm.set_value('total', total_amount);
 }
