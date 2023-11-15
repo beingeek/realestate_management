4ecac83b0fc9ca3f237e3a7808e2ebe32154fc09
Tariq Siddique <136646271+mtariqsiddique@users.noreply.github.com>
2023-11-15 15:46:35 +0500

    Update journal_entry.py

--- realestate_account/events/journal_entry.py
+++ realestate_account/events/journal_entry.py
@@ -12,3 +12,8 @@ def check_document_status(doc, method=None):
         cust_pmt = frappe.get_doc('Customer Payment', doc.get('custom_document_number'))
         if cust_pmt.document_number and not frappe.db.exists('Plot Booking', {'name': cust_pmt.document_number, 'status': 'Active'}):  
             frappe.throw(_('The {0} is not Active').format(frappe.get_desk_link('Plot Booking', cust_pmt.document_number)))
+
+    elif doc.get('custom_document_type') == 'Property Transfer' and doc.get('custom_document_number'):
+        cust_pmt = frappe.get_doc('Customer Payment', doc.get('custom_document_number'))
+        if cust_pmt.document_number and not frappe.db.exists('Property Transfer', {'name': cust_pmt.document_number, 'status': 'Active'}):  
+            frappe.throw(_('The {0} is not Active').format(frappe.get_desk_link('Property Transfer', cust_pmt.document_number)))
