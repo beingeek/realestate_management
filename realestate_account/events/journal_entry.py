0001bd29187beb7939943671e8aa5242718a2c3f
Tariq Siddique <136646271+mtariqsiddique@users.noreply.github.com>
2023-12-23 02:40:51 +0500

    Update journal_entry.py

--- realestate_account/events/journal_entry.py
+++ realestate_account/events/journal_entry.py
@@ -24,4 +24,18 @@ def validate_id_card_number_format(doc, method=None):
         if not re.match(pattern, tax_number):
             frappe.msgprint(_('Invalid CNIC format. Please enter a valid format eg. (44204-1010085-0)'))
             frappe.throw(_('Invalid CNIC format. Please enter a valid format eg. (44204-1010085-0)'))
+
+def validate_check_duplicate_cnic_number(doc, method=None):
+        if doc.id_card_no:
+            duplicate_id_card_no = frappe.get_value(
+                'Customer',
+                filters={
+                    'id_card_no': doc.id_card_no,
+                    'name': ('!=', doc.name),
+                    'docstatus': ('!=', 2)
+                },
+                fieldname='name'
+            )
+            if duplicate_id_card_no:
+                frappe.throw(_('Duplicate CNIC found in {0}').format(frappe.get_desk_link('Customer', duplicate_id_card_no)))
   
